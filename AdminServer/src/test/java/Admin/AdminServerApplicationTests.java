39f1bfd94e62f4d19f10f4405ddcb17370dcb1c4
Koti79k <Koteswararao791212@gmail.com>
2023-03-16 21:06:12 +0530

    spring boot admin server and client added

---
+++ AdminServer/src/test/java/Admin/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
